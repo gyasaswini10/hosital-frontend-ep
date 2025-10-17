0ba850d6b568192082ce4a0b177e5fde16fbaf23
gyasaswini10 <gyasu1110@gmail.com>
2025-10-17 19:22:10 +0530

    coding in react

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <Router>
+ <Router basename="/hospital">
       <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
